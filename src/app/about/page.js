37f869f8b5e4745cab8af59de0693eb27b0f4884
Jayedjb765 <jayedjb76@gmail.com>
2025-05-24 03:46:57 +0600

    font optimization

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'About Us',
+    description: 'Learn more about our company and our mission.',
+}
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>This is about page</h1>
+        </div>
+    );
+};
+
+export default AboutPage;
